fc640966f80f3114f9be7bff0824ed729ad8cb89
Hripsime <1996ripo@gmail.com>
2022-02-28 05:21:33 -0800

    ex.3 edit

--- script.js
+++ script.js
@@ -60,46 +60,62 @@
 
 // ========================3===============================
 
-// const bookSearch = document.getElementById('book-search');
-// const form = document.getElementById('form');
-// const submit = document.getElementById('submit');
-// const result = document.getElementById('result');
-// const pages = document.getElementById('pages');
+const bookSearch = document.getElementById('book-search');
+const form = document.getElementById('form');
+const submit = document.getElementById('submit');
+const result = document.getElementById('result');
+const pages = document.getElementById('pages');
 
-// form.addEventListener('click', e => {
-//     e.preventDefault();
-// });
+form.addEventListener('click', e => {
+    e.preventDefault();
+});
 
-// submit.addEventListener('click', e => {
-//     const value = bookSearch.value.split(' ').join('+');
 
-//     pages.innerHTML="";
+let pageNum = 1;
 
-//     fetch(`http://openlibrary.org/search.json?q=${value}`).then(res => {
-//         const json = res.json();
-//         return json;
-//     }
-//     ).then(
-//         json => {
-//             console.log(json);
-//             console.log(json.numFound);
-//             for (let i = 1; i <= Math.ceil(+json.numFound / 100); i++) {
-//                 pages.insertAdjacentHTML('beforeend', `<a href="#">${i}</a>`)
-//             }
+function searchFunction() {
+    const value = bookSearch.value.split(' ').join('+');
+    pages.innerHTML = `<a href="#">1</a>`;
+    result.innerHTML = "";
 
-//             for (const [key, value] of Object.entries(json.docs)) {
+    fetch(`http://openlibrary.org/search.json?q=${value}`).then(res => {
+        const json = res.json();
+        return json;
+    }).then(json => {
+        for (let i = 2; i <= Math.ceil(+json.numFound / 100); i++) {
+            pages.insertAdjacentHTML('beforeend', `<a href="#">${i}</a>`)
+        }
+    });
 
-//                 const li = (`<li>
-//                                 <p> Title - ${value.title} <br> First Publish Year - ${value.first_publish_year}</p>
-//                                 <p>Author Name - ${value.author_name}</p>
-//                                 <p>Subject - ${value.subject}</p> 
-//                            </li>`);
-//                 result.insertAdjacentHTML('beforeend', li)
-//             }
 
-//         }
-//     )
-// });
+    fetch(`http://openlibrary.org/search.json?q=${value}&page=${pageNum}`).then(res => {
+        console.log(res);
+        const json = res.json();
+        return json;
+    }
+    ).then(
+        json => {
+            console.log(json);
+            console.log(json.numFound);
+            for (const [key, value] of Object.entries(json.docs)) {
+                const li = (`<li>
+                                <p> Title - ${value.title} <br> First Publish Year - ${value.first_publish_year}</p>
+                                <p>Author Name - ${value.author_name}</p>
+                                <p>Subject - ${value.subject}</p> 
+                           </li>`);
+                result.insertAdjacentHTML('beforeend', li)
+            }
+
+        }
+    )
+}
+
+
+submit.addEventListener('click', searchFunction);
+pages.addEventListener('click', e => {
+    pageNum = e.target.textContent;
+    searchFunction();
+}, true)
 
 // ===============================4=========================
 // const resultN4 = document.getElementById('resultN4');
@@ -111,9 +127,9 @@
 //     const div = (`<div>
 //                                 <p> Title: ${json.title}
 //                                 <p>  Description:  ${json.description}</p>
-//                                 <p> Director: ${json.director}</p> 
-//                                 <p> Producer: ${json.producer}</p> 
-//                                 <p> Release Date: ${json.release_date}</p> 
+//                                 <p> Director: ${json.director}</p>
+//                                 <p> Producer: ${json.producer}</p>
+//                                 <p> Release Date: ${json.release_date}</p>
 
 //                            </div>`);
 //     resultN4.insertAdjacentHTML('beforeend', div)
